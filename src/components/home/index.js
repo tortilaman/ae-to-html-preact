2f9c34374d572520b8078103972c5203a3cb39cb
Nick Doering <nick@nickdoering.com>
2017-06-07 21:09:40 -0700

    Page now refreshes as new files are added.

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,5 +1,5 @@
 import { h, Component } from 'preact';
-var PouchDB = require('pouchdb');
+let PouchDB = require('pouchdb');
 
 import style from './style.less';
 import NewAnim from '../newAnim';
@@ -9,7 +9,7 @@ import Animation from '../animation';
 
 export default class Home extends Component {
 	constructor() {
-    super();
+		super();
 		this.db = new PouchDB('files');
 		this.state = {
 			db: "",
@@ -17,15 +17,17 @@ export default class Home extends Component {
 		};
 
 		this.db.allDocs({include_docs: true, attachments: true, binary: true}).then(result => {
-			if(result.total_rows > 0) {
+			if (result.total_rows > 0) {
 				return Promise.all(result.rows.map(row => {
 					return this.readFile(row);
-				}))
+				}));
 			}
 		}).then(arrayOfResults => {
 			return this.setData(arrayOfResults);
 		}).catch(console.log.bind(console));
-	}//End of constructor
+
+		this.addFile = this.addFile.bind(this);
+	}
 
 	/**
 	 *  Update file state with actual file data.
@@ -36,34 +38,24 @@ export default class Home extends Component {
 	 */
 	setData(resultsArr) {
 		return Promise.all(resultsArr.map(item => {
-			return this.setState((prevState, props) => {
-				prevState.files.push(item);
-			})
-		}))
-
-		if(this.changes) this.changes.cancel();
-		this.changes = db.changes({
-		  since: 'now',
-		  live: true,
-		  include_docs: true,
-			attachments: true,
-			binary: true
-		}).on('change', function(change) {
-			console.log("There have been changes to the database");
-			return Promise.all(change.map(item => {
-				return this.readFile(item);
-			})).then(arrayOfResults => {
-				return this.setData(arrayOfResults);
-			})
-		}).on('complete', function(info) {
-		  // changes() was canceled
-		}).on('error', function (err) {
-		  console.log(err);
-		});
+			return this.setState((prevState) => {
+				// prevState.files.push(item);
+				let ind = prevState.files.findIndex(exItem => {
+					return exItem.id === item.id;
+				});
+				if (ind >= 0) {
+					prevState.files[ind] = item;
+				} else {
+					prevState.files.push(item);
+				}
+			});
+		}));
 	}
 
 	/**
-	 *  Converts Blobs in PouchDB to strings for use by bodymovin..
+	 *  File Reader Promise Helper:
+	 *  Converts Blobs in PouchDB to strings for use by bodymovin.
+	 *  From https://gist.github.com/svnlto/5c62c0ecfcb741a5eef9
 	 *
 	 *  @method readFile
 	 *
@@ -72,38 +64,80 @@ export default class Home extends Component {
 	 *  @return {Promise}    Fulfilled after blob converted.
 	 */
 	readFile(row) {
-	  let reader = new global.FileReader();
-		let file = row.doc._attachments.filename.data;
+		let reader = new global.FileReader();
+		let doc = row.doc ? row.doc : row;
+		let file = doc._attachments.filename.data;
 
-	  return new Promise((resolve, reject) => {
-	    reader.onload = (event) => {
-	      let newFile = {
-					title: row.doc._id,
-					id: row.doc._id.replace(/ /g, ""),
+		return new Promise((resolve, reject) => {
+			reader.onload = (event) => {
+				let newFile = {
+					title: doc._id,
+					id: doc._id.replace(/ /g, ""),
 					data: JSON.parse(event.target.result)
 				};
-	      resolve(newFile);
-	    };
+				resolve(newFile);
+			};
 
-	    reader.onerror = () => {
-	      return reject(this);
-	    };
+			reader.onerror = () => {
+				return reject(this);
+			};
 
-	    if (/^image/.test(file.type)) {
-	      reader.readAsDataURL(file);
-	    } else {
-	      reader.readAsText(file);
-	    }
-	  })
+			if (/^image/.test(file.type)) {
+				reader.readAsDataURL(file);
+			} else {
+				reader.readAsText(file);
+			}
+		});
 	}//End of readFile
 
-
+	addFile(file) {
+		console.log("Adding file to state");
+		console.log(file);
+		return Promise.resolve(this.readFile(file)).then( file => {
+			//We're putting the file into an array here. Will cause problems if it's already an array...
+			let fileArr = [file];
+			this.setData(fileArr);
+		});
+	}
 
 	render() {
+
+
+		/*
+			Changes adds other garbage, it's more work don't use it.
+		 */
+		// this.changes = this.db.changes({
+		// 	since: 'now',
+		// 	live: true,
+		// 	include_docs: true,
+		// 	attachments: true,
+		// 	binary: true
+		// }).on('change', change => {
+		// 	console.log("There have been changes to the database");
+		// 	console.log(change);
+		// 	if (Array.isArray(change)) {
+		// 		//	Need to process multiple items
+		// 		//	This hasn't been tested...
+		// 		return Promise.all(change.map(item => {
+		// 			return this.readFile(item);
+		// 		})).then(arrayOfResults => {
+		// 			return this.setData(arrayOfResults);
+		// 		});
+		// 	} else {
+		// 		return this.setState((prevState) => {
+		// 			prevState.files.push(change);
+		// 		});
+		// 	}
+		// }).on('complete', info => {
+		// 	// changes() was canceled
+		// }).on('error', (err) => {
+		// 	console.log(err);
+		// });
+
 		return (
 			<div class={style.home}>
 				<h1>Animations</h1>
-				<NewAnim localDB={this.db} />
+				<NewAnim localDB={this.db} addFile={this.addFile} />
 				<table>
 					{
 						this.state.files ? (
--- src/components/newAnim/index.js
+++ src/components/newAnim/index.js
@@ -1,65 +1,69 @@
 import { h, Component } from 'preact';
-var PouchDB = require('pouchdb');
+// let PouchDB = require('pouchdb');
 
 import style from './style.less';
 
 export default class NewAnim extends Component {
-	constructor(localDB) {
-    super();
-    this.state = {
+	constructor(props) {
+		super();
+		this.state = {
 			title: "",
 			file: null
 		};
-		this.animDb = localDB.localDB;
+		this.animDb = props.localDB;
 		this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
 	handleInputChange(event) {
 		const target = event.target;
-    const value = target.value;
-    const name = target.name;
+		const value = target.value;
+		const name = target.name;
 
 		this.setState({ [name]: value });
 	}
 
 	handleSubmit(event) {
 		event.preventDefault();
-		var nonJson = false;
-		var fileInput = document.getElementById('fileUpload');
-		for (var file of fileInput.files) {
-			if (file.type != 'application/json') {
+		let fileInput = document.getElementById('fileUpload');
+		for (let file of fileInput.files) {
+			if (file.type !== 'application/json') {
+				//Not JSON
 				alert("Non JSON file of type " + file.type + " was not uploaded");
 			} else {
-				var fileData = {
-			    _id: this.state.title,
-			    _attachments: {
-			      filename: {
+				//JSON file
+				let fileData = {
+					_id: this.state.title,
+					_attachments: {
+						filename: {
 							content_type: file.type,
-			        data: file
-			      }
-			    }
-			  }
-				//Get any existing document
-				var existingDoc = this.animDb.get(fileData._id).catch(err => {
-					//If it's a new file, return the new file instead of the old one.
-					if(err.name === 'not_found') {
-						return fileData;
-					} else {
-						throw err;
+							data: file
+						}
 					}
+				};
+				//Get existing document
+				let existingDoc = this.animDb.get(fileData._id).catch(err => {
+					//If an existing file wasn't found, return the new one.
+					if (err.name === 'not_found') {
+						return fileData;
+					}
+					//Some other unexpected error
+					throw err;
 				}).then(doc => {
-						doc.atachments = fileData.attachments;
-						return this.animDb.put(doc);
+					doc._attachments = fileData._attachments;
+					this.props.addFile(doc);
+					return this.animDb.put(doc);
 				}).then(() => {
-				  //Success
+					//Success
+					return console.log("Successfully added document");
 				}).catch(err => {
 					//New unexpected error, not 404 or 409
+					console.log(err);
 					console.log.bind(console);
-			  });
+				});
 			}
 		}
-  }
+	}
 
 	render() {
 		return (
